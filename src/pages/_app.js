e5202a4b17df38c64da44d58c88b9d740505da56
= <danillyudhistira@gmail.com>
2023-10-08 21:13:06 +0700

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
+import dynamic from 'next/dynamic';
+const BarNav =  dynamic(() => import('@/components/BarNav'));
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // const BarApp = Component?.appBar || (({ children }) => <AppBar />)
+
+  return (
+    <>
+      <BarNav />
+      <div style={{ marginBottom: '2vw' }} />
+      <Component {...pageProps} />
+    </>
+  );
 }
